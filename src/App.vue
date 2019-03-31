bf1ae85987fe031a473d56d8a902633becec9b39
ludger.pottmeier@atos.net <ludger.pottmeier@atos.net>
2019-03-31 17:37:33 +0200

    runs

--- src/App.vue
+++ src/App.vue
@@ -6,29 +6,30 @@
 </template>
 
 <script>
+import Firebase from 'firebase'
 export default {
   name: 'App'
 }
-import Firebase from 'firebase'
 
 // Initialize Firebase
 let config = {
-    apiKey: "AIzaSyCqxeB7SI7rIe5mI3LY0Rpi4qnvJyqt13w",
-    authDomain: "knobelapp.firebaseapp.com",
-    databaseURL: "https://knobelapp.firebaseio.com",
-    projectId: "knobelapp",
-    storageBucket: "knobelapp.appspot.com",
-    messagingSenderId: "242600715534"
-};
-  
+  apiKey: 'AIzaSyCqxeB7SI7rIe5mI3LY0Rpi4qnvJyqt13w',
+  authDomain: 'knobelapp.firebaseapp.com',
+  databaseURL: 'https://knobelapp.firebaseio.com',
+  projectId: 'knobelapp',
+  storageBucket: 'knobelapp.appspot.com',
+  messagingSenderId: '242600715534'
+}
+
 let app = Firebase.initializeApp(config)
 let db = app.database()
+// eslint-disable-next-line
 let jungsRef = db.ref('jungs')
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
