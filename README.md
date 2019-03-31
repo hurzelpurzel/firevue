9f69437975ec4a0554f100b3e5d02c9d344b9803
ludger.pottmeier@atos.net <ludger.pottmeier@atos.net>
2019-03-31 17:23:16 +0200

    initial

---
+++ README.md
@@ -0,0 +1,5 @@
+# firevue
+Knobelapp on firebase with vue
+
+See
+https://medium.com/codingthesmartway-com-blog/vue-js-2-firebase-e4b2479e35a8
---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+import Firebase from 'firebase'
+
+// Initialize Firebase
+let config = {
+    apiKey: "AIzaSyCqxeB7SI7rIe5mI3LY0Rpi4qnvJyqt13w",
+    authDomain: "knobelapp.firebaseapp.com",
+    databaseURL: "https://knobelapp.firebaseio.com",
+    projectId: "knobelapp",
+    storageBucket: "knobelapp.appspot.com",
+    messagingSenderId: "242600715534"
+};
+  
+let app = Firebase.initializeApp(config)
+let db = app.database()
+let jungsRef = db.ref('jungs')
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
